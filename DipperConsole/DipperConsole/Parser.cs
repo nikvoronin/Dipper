5ecc87bb0159a5aeb0eee4562383037078549f65
nikvoronin <nvoronin@outlook.com>
2019-09-03 23:07:49 +0300

    all about xkcd: png, img and url menu types. gopher.floodgap.com/1/fun/xkcd/

--- DipperConsole/DipperConsole/Parser.cs
+++ DipperConsole/DipperConsole/Parser.cs
@@ -15,14 +15,14 @@ namespace DipperConsole
                 if (line.Length == 0 || (line.Length == 1 && line[0] == '.'))
                     break;
 
-                char t = line[0];
+                var t = ToGopherItem(line[0]);
 
                 string[] tabbed = line.Split('\t');
                 string textLine = tabbed[0].Substring(1);
 
                 var stdForeColor = Console.ForegroundColor;
                 switch (t) {
-                    case '0':
+                    case GopherItemType.TextDocument:
                         Console.ForegroundColor = ConsoleColor.DarkGray;
                         Console.Write($"[DOC] ");
                         Console.ForegroundColor = stdForeColor;
@@ -31,7 +31,7 @@ namespace DipperConsole
                         Console.WriteLine($"{tabbed[2]}/0{tabbed[1]}");
                         Console.ForegroundColor = stdForeColor;
                         break;
-                    case '1':
+                    case GopherItemType.SubmenuDir:
                         Console.ForegroundColor = ConsoleColor.DarkGray;
                         Console.Write($"[DIR] ");
                         Console.ForegroundColor = stdForeColor;
@@ -39,11 +39,39 @@ namespace DipperConsole
                         Console.WriteLine($"{tabbed[2]}/1{tabbed[1]}");
                         Console.ForegroundColor = stdForeColor;
                         break;
+                    case GopherItemType.GifImage:
+                    case GopherItemType.ImageFile:
+                    case GopherItemType.PngFile:
+                        Console.ForegroundColor = ConsoleColor.DarkGray;
+                        Console.Write($"[IMG] ");
+                        Console.ForegroundColor = stdForeColor;
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine($"{tabbed[2]}/0{tabbed[1]}");
+                        Console.ForegroundColor = stdForeColor;
+                        break;
+
+                    case GopherItemType.HtmlFile:
+                        string htmlurl = tabbed[1].Replace("URL:", "");
+                        Console.ForegroundColor = ConsoleColor.DarkGray;
+                        Console.Write($"[URL] ");
+                        Console.ForegroundColor = stdForeColor;
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.WriteLine($"{htmlurl}");
+                        Console.ForegroundColor = stdForeColor;
+                        break;
+
                     default:
                         Console.WriteLine($"      {textLine}");
                         break;
                 }
             }
         }
+        public static GopherItemType ToGopherItem(int ch)
+        {
+            return Enum.IsDefined(typeof(GopherItemType), ch) ?
+                (GopherItemType)ch :
+                GopherItemType.None;
+        }
+
     }
 }
